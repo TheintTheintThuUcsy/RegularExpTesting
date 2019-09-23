855b53c8bf2a6711fa80061050f376be379b4220
TheintTheintThu <theinttheintthu.ucsy@gmail.com>
2019-09-23 13:56:28 +0630

    add coding @Pattern to bean class(Employee.java)

--- RegularExpTesting/src/main/java/com/javatpoint/Employee.java
+++ RegularExpTesting/src/main/java/com/javatpoint/Employee.java
@@ -1,12 +1,14 @@
 package com.javatpoint;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 public class Employee {
 	private String name;
 	@NotNull
 	@Size(min = 1, message = "required")
+	@Pattern(regexp = "^[a-zA-Z0-9]{3}", message = "length must be 3")
 	private String pass;
 
 	public String getName() {
